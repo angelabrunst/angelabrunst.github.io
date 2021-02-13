255d3780582cd7f2758e1c949916de44603477d6
angelabrunst <angela.lynne.brunst@gmail.com>
2021-02-13 09:14:18 -0700

    Add wind chill

--- lesson_6/js/script.js
+++ lesson_6/js/script.js
@@ -22,3 +22,14 @@ function currentDate() {
     };
     document.getElementById("currentDate").textContent = todaysDate.toLocaleDateString('en-us', options);
 }
+
+//windchill calculator
+function windChill() {
+    var temp = 25
+    var windSpeed = 5
+    var chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+
+    chill = Math.round(chill * 1) / 1;
+
+    document.getElementById('windChill').innerHTML = chill;
+}
