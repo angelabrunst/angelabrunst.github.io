d490dba74dfdadaae87a63107c9262213211e15b
angelabrunst <angela.lynne.brunst@gmail.com>
2021-03-04 13:44:19 -0700

    fix css

--- lesson_9/prophets/prophets.js
+++ lesson_9/prophets/prophets.js
@@ -5,16 +5,26 @@ fetch(requestURL)
         return response.json();
     })
     .then(function(jsonObject) {
-        console.table(jsonObject);
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const prophets = jsonObject['prophets'];
         for (let i = 0; i < prophets.length; i++) {
-            const prophets = jsonObject['prophets'];
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            let p2 = document.createElement('p');
+            let img = document.createElement('img');
+
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+            p2.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
+            img.setAttribute('src', prophets[i].imageurl);
+            img.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' — ' + prophets[i].order)
+
+            card.appendChild(h2);
+            card.appendChild(p);
+            card.appendChild(p2);
+            card.appendChild(img);
+
+            document.querySelector('div.cards').appendChild(card);
         }
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-        card.appendChild(h2);
-
-        document.querySelector('div.cards').appendChild(card);
     });
