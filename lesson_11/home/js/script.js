51f8ed3d2c739e92ea4aa11b4d8bbfe7234313d3
angelabrunst <angela.lynne.brunst@gmail.com>
2021-03-12 22:04:32 -0700

    fix home errors

--- lesson_11/home/js/script.js
+++ lesson_11/home/js/script.js
@@ -15,6 +15,32 @@ function currentDate() {
     document.getElementById("currentDate").textContent = todaysDate.toLocaleDateString('en-us', options);
 }
 
+//Progressive Loading for Images
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
 //town data
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
